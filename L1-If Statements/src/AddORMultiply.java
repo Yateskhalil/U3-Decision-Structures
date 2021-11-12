59b589878d4c339c5373fcfbc1f58a3782656f2e
yateskha000 <yateskha000@warren.k12.in.us>
2021-11-12 12:00:12 -0500

    Completed notes

--- L1-If Statements/src/AddORMultiply.java
+++ L1-If Statements/src/AddORMultiply.java
@@ -1,10 +1,59 @@
+import java.util.Scanner;
+
 public class AddORMultiply {
 
     /*
     Write two methods, one that adds two numbers and second that multiplies them.
     Then, write a main method that prompts the user to enter 2 numbers.
 
-    If the first one is larger than the second, mutiply and display the result
+    If the first one is larger than the second, multiply and display the result
     If not, add them and display the results.
      */
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+
+        System.out.print("Please enter your First number:");
+       double  num1 = read.nextDouble();
+        System.out.print("Please enter your Second number:");
+       double num2 = read.nextDouble();
+
+        add(num1,num2);
+        multiply(num1,num2);
+    }
+
+    public static void multiply(double num1,double num2){
+        if (num1>num2){
+
+            System.out.println(num1*num2);
+        }
+
+
+
+
+
+    }
+
+    public static void add(double num1, double num2){
+        if (num1<num2){
+
+            System.out.println(num1+num2);
+        }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
